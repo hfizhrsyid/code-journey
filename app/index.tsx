1a3ddc584df7c14f83456f4e57dd060b9aeeb088
fathnhh <fathnh18@gmail.com>
2025-11-08 20:59:49 +0700

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen. Halooo! Coba-coba</Text>
+      <Text>Halooo! coba</Text>
     </View>
   );
 }
